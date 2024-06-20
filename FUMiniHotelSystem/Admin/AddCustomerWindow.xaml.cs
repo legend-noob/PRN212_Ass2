2a371666cdad4a9f5e7905b7077281bcfe6d9a89
duydk1111 <duythenoob@gmail.com>
2024-06-20 23:42:21 +0700

    assignment 2

---
+++ FUMiniHotelSystem/Admin/AddCustomerWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using BusinessObject;
+using DataAccess.Repository;
+
+namespace FUMiniHotelSystem.Admin
+{
+    public partial class AddCustomerWindow : Window
+    {
+        private CustomerRepository repository;
+        private BusinessObject.Customer customer;
+        private CustomerObject customerObject;
+        DateTime dob;
+
+        public AddCustomerWindow(BusinessObject.Customer existCustomer = null)
+        {
+            InitializeComponent();
+            repository = new CustomerRepository();
+            customer = existCustomer ?? new BusinessObject.Customer();
+            customerObject = new CustomerObject();
+            if (existCustomer != null)
+            {
+
+                tbEmail.Text = customer.EmailAddress;
+                tbPass.Text = customer.Password;
+                tbName.Text = customer.CustomerFullName;
+                tbPhone.Text = customer.Telephone;
+                dob = (DateTime)customer.CustomerBirthday;
+            }
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string email = tbEmail.Text;
+                string password = tbPass.Text;
+                string name = tbName.Text;
+                string phone = tbPhone.Text;
+                DateTime dob = (DateTime)dpDob.SelectedDate;
+
+                if (!customerObject.ValidateString(email))
+                {
+                    MessageBox.Show("Please enter a valid email address.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (!customerObject.ValidateString(password))
+                {
+                    MessageBox.Show("Please enter a valid password.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (!customerObject.ValidateString(name))
+                {
+                    MessageBox.Show("Please enter a valid name.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+
+                customer.EmailAddress = email;
+                customer.Password = password;
+                customer.CustomerFullName = name;
+                customer.Telephone = phone;
+                customer.CustomerBirthday = dob;
+
+                if (customer.CustomerId == 0)
+                {
+                    repository.Insert(customer);
+                    MessageBox.Show("Customer added.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    repository.Update(customer);
+                    MessageBox.Show("Customer updated.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while saving: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ FUMiniHotelSystem/WindowLogin.xaml.cs
@@ -0,0 +1,77 @@
+﻿using BusinessObject;
+using DataAccess.Repository;
+using FUMiniHotelSystem.Admin;
+using FUMiniHotelSystem.Customer;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FUMiniHotelSystem
+{
+    /// <summary>
+    /// Interaction logic for WindowLogin.xaml
+    /// </summary>
+    public partial class WindowLogin : Window
+    {
+        private IConfigurationRoot configuration;
+        private CustomerRepository customerRepository = new CustomerRepository();
+        public WindowLogin()
+        {
+            var builder = new ConfigurationBuilder()
+                           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            configuration = builder.Build();
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Password;
+
+            BusinessObject.Customer customer = customerRepository.GetUserByEmailAndPassword(email, password);
+
+            string adminEmail = configuration.GetSection("Admin:Email").Value;
+            string adminPassword = configuration.GetSection("Admin:Password").Value;
+			string customerEmail = configuration.GetSection("Customer:Email").Value;
+			string customerPassword = configuration.GetSection("Customer:Password").Value;
+			if (email.Equals(customerEmail) && password.Equals(customerPassword))
+            {
+                Application.Current.Properties["LoggedInUser"] = customerRepository.GetUserByEmailAndPassword(email, password);
+                MessageBox.Show("Welcome Customer");
+                WindowCustomer customerPage = new WindowCustomer();
+                customerPage.Show();
+                this.Close();
+            }            
+            else
+            if (email.Equals(adminEmail) && password.Equals(adminPassword))
+            {
+                MessageBox.Show("Welcome admin");
+                WindowAdmin admin = new WindowAdmin();
+                admin.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect email/password");
+            }
+        }
+
+		private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+	}
+}
